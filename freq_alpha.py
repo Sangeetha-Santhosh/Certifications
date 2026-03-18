f12e185fe328dea268f6e176a88b3a2e279b8489
Sangeetha-Santhosh <sangeethasan8222@gmail.com>
2026-03-18 22:39:34 +0530

    Delete freq_alpha.py

--- freq_alpha.py
+++
@@ -1,6 +0,0 @@
-#frequency of alphabets in word
-text = input("Enter a string: ").lower()
-for ch in set(text):
-    if ch.isalpha():  
-        count = text.count(ch)
-        print(ch, ":", count)
